bffade9ca70226527bb66979791515266a766539
reichert-splinterlands <timo@splinterlands.com>
2022-06-22 21:49:07 +0200

    update example

--- src/example/beacon.example.ts
+++ src/example/beacon.example.ts
@@ -2,39 +2,62 @@ import { Client } from '../index';
 import { log, timeout } from '../utils';
 
 const main = async () => {
-    // No config client
+    /**
+     * Client 0 with no config
+     * Beacon service is used to load the best RPC nodes
+     * Nodes are refreshed every 300 seconds (5 Minutes) after loadNodes()
+     */
     const client0 = new Client();
     await client0.loadNodes();
     log(`0 - No config client: ${(await client0.database.getAccount('splinterlands')).name}`);
     await timeout(3 * 1000);
 
-    // Client with pre-set nodes only
+    /**
+     * Client A with pre-defined nodes
+     * Beacon service is NOT used
+     */
     const clientA = new Client({ nodes: ['wrong.hive-api.com', 'hived.splinterlands.com', 'hived-2.splinterlands.com'] });
-    log(`A - Client with pre-set nodes only: ${(await clientA.database.getAccount('splinterlands')).name}`);
+    log(`A - Client: ${(await clientA.database.getAccount('splinterlands')).name}`);
     await timeout(3 * 1000);
 
-    // Client with pre-set set nodes as well as manual beacon nodes loading and WITH interval
+    /**
+     * Client B with pre-defined nodes
+     * Beacon service is used to load the best RPC nodes
+     * Nodes are refreshed every 2 seconds after loadNodes()
+     */
     const clientB = new Client({ nodes: ['hived.splinterlands.com', 'hived-2.splinterlands.com'], beacon: { intervalTime: 2 } });
-    log(`B - Client with pre-set set nodes as well as manual beacon nodes loading and WITH interval: ${(await clientB.database.getAccount('splinterlands')).name}`);
     await clientB.loadNodes();
+    log(`B - Client: ${(await clientB.database.getAccount('splinterlands')).name}`);
     await timeout(5 * 1000);
     clientB.destroy(); // Clears intervals
 
-    // Client with pre-set set nodes as well as manual beacon nodes loading but WITHOUT interval
+    /**
+     * Client C with pre-defined nodes
+     * Beacon service is used to load the best RPC nodes
+     * Nodes are NOT refreshed due to 'manual' mode
+     */
     const clientC = new Client({ nodes: ['hived.splinterlands.com', 'hived-2.splinterlands.com'], beacon: { mode: 'manual' } });
-    log(`C - Client with pre-set set nodes as well as manual beacon nodes loading but WITHOUT interval: ${(await clientC.database.getAccount('splinterlands')).name}`);
     await clientC.loadNodes();
+    log(`C - Client: ${(await clientC.database.getAccount('splinterlands')).name}`);
     await timeout(5 * 1000);
 
-    // Client with pre-set set nodes as well as loadOnInitialize beacon nodes loading and WITH interval
+    /**
+     * Client D with pre-defined nodes
+     * Beacon service is used ON NEW CLIENT (due to loadOnInitialize) to load the best RPC nodes
+     * Nodes are refreshed every 2 seconds after new Client()
+     */
     const clientD = new Client({ nodes: ['hived.splinterlands.com', 'hived-2.splinterlands.com'], beacon: { intervalTime: 2, loadOnInitialize: true } });
-    log(`D - Client with pre-set set nodes as well as loadOnInitialize beacon nodes loading and WITH interval: ${(await clientD.database.getAccount('splinterlands')).name}`);
+    log(`D - Client: ${(await clientD.database.getAccount('splinterlands')).name}`);
     await timeout(5 * 1000);
     clientD.destroy(); // Clears intervals
 
-    // Client with pre-set set nodes as well as loadOnInitialize beacon nodes loading and WITH interval
+    /**
+     * Client E with NO pre-defined nodes
+     * Beacon service is used ON NEW CLIENT (due to loadOnInitialize) to load the best RPC nodes
+     * Nodes are refreshed every 2 seconds after new Client()
+     */
     const clientE = new Client({ beacon: { intervalTime: 2, loadOnInitialize: true } });
-    log(`E - Client only with beacon nodes and WITH interval: ${(await clientE.database.getAccount('splinterlands')).name}`);
+    log(`E - Client: ${(await clientE.database.getAccount('splinterlands')).name}`);
     await timeout(5 * 1000);
     clientE.destroy(); // Clears intervals
 
