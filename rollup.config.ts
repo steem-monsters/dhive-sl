75c5093ced38a61b28fe284d20e176c4083cecb5
reichert-splinterlands <timo@splinterlands.com>
2022-07-05 08:40:16 +0200

    update rollup, no browser-bundle for now

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,7 @@
-// import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
+// import nodePolyfills from 'rollup-plugin-polyfill-node';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 const name = 'dhive-sl';
 
@@ -13,7 +13,7 @@ const bundle = (config) => ({
 
 export default [
     bundle({
-        plugins: [esbuild({ minify: true })],
+        plugins: [nodeResolve(), esbuild({ minify: true })],
         output: [
             {
                 file: `dist/${name}.js`,
@@ -21,41 +21,59 @@ export default [
                 sourcemap: true,
             },
             {
-                file: `dist/${name}.mjs`,
+                file: `dist/${name}.esm.js`,
                 format: 'es',
                 sourcemap: true,
             },
         ],
     }),
-    // TODO: Not yet fully working with polyfills
-    bundle({
-        plugins: [nodePolyfills({ include: ['stream', 'assert'] }), esbuild({ minify: true })],
-        output: [
-            {
-                name: 'dhiveSL',
-                file: `dist/${name}.umd`,
-                format: 'umd',
-                sourcemap: true,
-                globals: {
-                    stream: 'stream',
-                    bytebuffer: 'bytebuffer',
-                    assert: 'assert',
-                    crypto: 'crypto',
-                    secp256k1: 'secp256k1',
-                    bs58: 'bs58',
-                    ecurve: 'ecurve',
-                    bigi: 'bigi',
-                    jsbi: 'jsbi',
-                    fs: 'fs',
-                    'splinterlands-hive-engine': 'splinterlandsHiveEngine',
-                    verror: 'verror',
-                    'secure-random': 'secureRandom',
-                    'browserify-aes': 'browserifyAes',
-                    'cross-fetch': 'crossFetch',
-                },
-            },
-        ],
-    }),
+    // // TODO: Not yet fully working with polyfills
+    // bundle({
+    //     external: ['buffer'],
+    //     plugins: [
+    //         nodePolyfills({
+    //             include: ['buffer', 'stream', 'events', 'assert'],
+    //         }),
+    //         nodeResolve({
+    //             browser: true,
+    //             preferBuiltins: false,
+    //         }),
+    //         ,
+    //         esbuild({ minify: false }),
+    //     ],
+    //     output: [
+    //         {
+    //             name: 'dhiveSL',
+    //             file: `dist/${name}.esm.browser.js`,
+    //             format: 'es',
+    //             sourcemap: true,
+    //         },
+    //         {
+    //             name: 'dhiveSL',
+    //             file: `dist/${name}.umd.js`,
+    //             format: 'umd',
+    //             sourcemap: true,
+    //             globals: {
+    //                 buffer: '_buffer',
+    //                 stream: 'stream',
+    //                 bytebuffer: 'bytebuffer',
+    //                 assert: 'assert',
+    //                 crypto: 'crypto',
+    //                 secp256k1: 'secp256k1',
+    //                 bs58: 'bs58',
+    //                 ecurve: 'ecurve',
+    //                 bigi: 'bigi',
+    //                 jsbi: 'jsbi',
+    //                 fs: 'fs',
+    //                 'splinterlands-hive-engine': 'splinterlandsHiveEngine',
+    //                 verror: 'verror',
+    //                 'secure-random': 'secureRandom',
+    //                 'browserify-aes': 'browserifyAes',
+    //                 'cross-fetch': 'crossFetch',
+    //             },
+    //         },
+    //     ],
+    // }),
     bundle({
         plugins: [dts()],
         output: {
