3a5e3550cc41e333ca5871096ec690eda890b62e
reichert-splinterlands <timo@splinterlands.com>
2022-07-27 11:02:48 +0200

    fixed exports and ordered exports correctly

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
 import * as utils from './utils';
 export { utils };
 
+export * from './modules/beacon';
 export * from './modules/blockchain';
-export * from './modules/database';
-export * from './modules/rc';
-export * from './modules/key';
-export * from './modules/hivemind';
-export * from './modules/engine';
-export * from './modules/transaction';
 export * from './modules/broadcast';
+export * from './modules/database';
+export * from './modules/engine';
+export * from './modules/hivemind';
+export * from './modules/key';
+export * from './modules/operation';
+export * from './modules/rc';
+export * from './modules/transaction';
 
+export * from './chain/keys/keys';
+export * from './chain/keys/utils';
 export * from './chain/account';
 export * from './chain/asset';
 export * from './chain/block';
 export * from './chain/comment';
+export * from './chain/hivemind';
+export * from './chain/memo';
 export * from './chain/misc';
 export * from './chain/operation';
+export * from './chain/rc';
 export * from './chain/serializer';
 export * from './chain/transaction';
-export * from './chain/keys/keys';
-export * from './chain/keys/utils';
-export * from './chain/memo';
 
 export * from './client';
 export * from './crypto';
