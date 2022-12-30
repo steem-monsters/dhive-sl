4fdd611e56d17f45471a402a77d0b0bb247e4be7
reichert-splinterlands <101267893+reichert-splinterlands@users.noreply.github.com>
2022-12-30 23:03:18 +0100

    removed unn. code

--- src/crypto/ecc.ts
+++ src/crypto/ecc.ts
@@ -1,102 +1,18 @@
-// Taken & modified from
-// https://github.com/cryptocoinjs/ecurve
+// source: https://github.com/cryptocoinjs/ecurve
 
 import BigInteger from 'bigi';
 
-export type EccCurveName = 'secp128r1' | 'secp160k1' | 'secp160r1' | 'secp192k1' | 'secp192r1' | 'secp256k1' | 'secp256r1';
-
-const ECC_CURVES = {
-    secp128r1: {
-        p: 'fffffffdffffffffffffffffffffffff',
-        a: 'fffffffdfffffffffffffffffffffffc',
-        b: 'e87579c11079f43dd824993c2cee5ed3',
-        n: 'fffffffe0000000075a30d1b9038a115',
-        h: '01',
-        Gx: '161ff7528b899b2d0c28607ca52c5b86',
-        Gy: 'cf5ac8395bafeb13c02da292dded7a83',
-    },
-    secp160k1: {
-        p: 'fffffffffffffffffffffffffffffffeffffac73',
-        a: '00',
-        b: '07',
-        n: '0100000000000000000001b8fa16dfab9aca16b6b3',
-        h: '01',
-        Gx: '3b4c382ce37aa192a4019e763036f4f5dd4d7ebb',
-        Gy: '938cf935318fdced6bc28286531733c3f03c4fee',
-    },
-    secp160r1: {
-        p: 'ffffffffffffffffffffffffffffffff7fffffff',
-        a: 'ffffffffffffffffffffffffffffffff7ffffffc',
-        b: '1c97befc54bd7a8b65acf89f81d4d4adc565fa45',
-        n: '0100000000000000000001f4c8f927aed3ca752257',
-        h: '01',
-        Gx: '4a96b5688ef573284664698968c38bb913cbfc82',
-        Gy: '23a628553168947d59dcc912042351377ac5fb32',
-    },
-    secp192k1: {
-        p: 'fffffffffffffffffffffffffffffffffffffffeffffee37',
-        a: '00',
-        b: '03',
-        n: 'fffffffffffffffffffffffe26f2fc170f69466a74defd8d',
-        h: '01',
-        Gx: 'db4ff10ec057e9ae26b07d0280b7f4341da5d1b1eae06c7d',
-        Gy: '9b2f2f6d9c5628a7844163d015be86344082aa88d95e2f9d',
-    },
-    secp192r1: {
-        p: 'fffffffffffffffffffffffffffffffeffffffffffffffff',
-        a: 'fffffffffffffffffffffffffffffffefffffffffffffffc',
-        b: '64210519e59c80e70fa7e9ab72243049feb8deecc146b9b1',
-        n: 'ffffffffffffffffffffffff99def836146bc9b1b4d22831',
-        h: '01',
-        Gx: '188da80eb03090f67cbf20eb43a18800f4ff0afd82ff1012',
-        Gy: '07192b95ffc8da78631011ed6b24cdd573f977a11e794811',
-    },
-    secp256k1: {
-        p: 'fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f',
-        a: '00',
-        b: '07',
-        n: 'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141',
-        h: '01',
-        Gx: '79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798',
-        Gy: '483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8',
-    },
-    secp256r1: {
-        p: 'ffffffff00000001000000000000000000000000ffffffffffffffffffffffff',
-        a: 'ffffffff00000001000000000000000000000000fffffffffffffffffffffffc',
-        b: '5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b',
-        n: 'ffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551',
-        h: '01',
-        Gx: '6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296',
-        Gy: '4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5',
-    },
-};
-
 export class EccPoint {
-    public curve;
-    public x: BigInteger;
-    public y: BigInteger;
-    public z: BigInteger;
     public compressed: boolean;
-
     private _zInv: any;
 
-    constructor(curve, x, y, z) {
-        // assert.notStrictEqual(z, undefined, 'Missing Z coordinate');
-
-        this.curve = curve;
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    constructor(public readonly curve: EccCurve, public x: BigInteger, public y: BigInteger, public z?: BigInteger) {
         this._zInv = null;
-
         this.compressed = true;
     }
 
     get zInv() {
-        if (this._zInv === null) {
-            this._zInv = this.z.modInverse(this.curve.p);
-        }
-
+        if (this._zInv === null) this._zInv = this.z.modInverse(this.curve.p);
         return this._zInv;
     }
 
@@ -112,26 +28,8 @@ export class EccPoint {
         return new EccPoint(curve, x, y, BigInteger.ONE);
     }
 
-    public equal(other) {
-        if (other === this) return true;
-        if (this.curve.isInfinity(this)) return this.curve.isInfinity(other);
-        if (this.curve.isInfinity(other)) return this.curve.isInfinity(this);
-
-        // u = Y2 * Z1 - Y1 * Z2
-        const u = other.y.multiply(this.z).subtract(this.y.multiply(other.z)).mod(this.curve.p);
-
-        if (u.signum() !== 0) return false;
-
-        // v = X2 * Z1 - X1 * Z2
-        const v = other.x.multiply(this.z).subtract(this.x.multiply(other.z)).mod(this.curve.p);
-
-        return v.signum() === 0;
-    }
-
     public negate() {
-        const y = this.curve.p.subtract(this.y);
-
-        return new EccPoint(this.curve, this.x, y, this.z);
+        return new EccPoint(this.curve, this.x, this.curve.p.subtract(this.y), this.z);
     }
 
     public add(b) {
@@ -206,7 +104,6 @@ export class EccPoint {
     }
 
     // Simple NAF (Non-Adjacent Form) multiplication algorithm
-    // TODO: modularize the multiplication algorithm
     multiply(k) {
         if (this.curve.isInfinity(this)) return this;
         if (k.signum() === 0) return this.curve.infinity;
@@ -233,29 +130,6 @@ export class EccPoint {
         return R;
     }
 
-    multiplyTwo(j, x, k) {
-        let i = Math.max(j.bitLength(), k.bitLength()) - 1;
-        let R = this.curve.infinity;
-        const both = this.add(x);
-
-        while (i >= 0) {
-            const jBit = j.testBit(i);
-            const kBit = k.testBit(i);
-
-            R = R.twice();
-
-            if (jBit) {
-                if (kBit) R = R.add(both);
-                else R = R.add(this);
-            } else if (kBit) {
-                R = R.add(x);
-            }
-            --i;
-        }
-
-        return R;
-    }
-
     public getEncoded(compressed: boolean | null = null): Uint8Array {
         if (compressed == null) compressed = this.compressed;
         if (this.curve.isInfinity(this)) return new Uint8Array(1).fill(0); // Infinity point encoded is simply '00'
@@ -273,7 +147,7 @@ export class EccPoint {
         return buffer;
     }
 
-    public static decodeFrom(curve, buffer: Uint8Array): EccPoint {
+    public static decodeFrom(curve: EccCurve, buffer: Uint8Array): EccPoint {
         const type = buffer[0];
         const compressed = type !== 4;
 
@@ -297,12 +171,6 @@ export class EccPoint {
         Q.compressed = compressed;
         return Q;
     }
-
-    public toString() {
-        if (this.curve.isInfinity(this)) return '(INFINITY)';
-
-        return '(' + this.affineX.toString() + ',' + this.affineY.toString() + ')';
-    }
 }
 
 export class EccCurve {
@@ -319,17 +187,20 @@ export class EccCurve {
         this.h = h;
 
         this.infinity = new EccPoint(this, null, null, BigInteger.ZERO);
-
-        // result caching
         this.pOverFour = p.add(BigInteger.ONE).shiftRight(2);
-
-        // determine size of p in bytes
         this.pLength = Math.floor((this.p.bitLength() + 7) / 8);
     }
 
-    public static fromCurveName(name: EccCurveName) {
-        const curve = ECC_CURVES[name];
-        if (!curve) return null;
+    public static secp256k1() {
+        const curve = {
+            p: 'fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f',
+            a: '00',
+            b: '07',
+            n: 'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141',
+            h: '01',
+            Gx: '79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798',
+            Gy: '483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8',
+        };
 
         const p = new BigInteger(curve.p, 16, undefined);
         const a = new BigInteger(curve.a, 16, undefined);
@@ -358,37 +229,6 @@ export class EccCurve {
 
         return Q.z.signum() === 0 && Q.y.signum() !== 0;
     }
-
-    public isOnCurve(Q) {
-        if (this.isInfinity(Q)) return true;
-
-        const x = Q.affineX;
-        const y = Q.affineY;
-        const a = this.a;
-        const b = this.b;
-        const p = this.p;
-
-        // Check that xQ and yQ are integers in the interval [0, p - 1]
-        if (x.signum() < 0 || x.compareTo(p) >= 0) return false;
-        if (y.signum() < 0 || y.compareTo(p) >= 0) return false;
-
-        // and check that y^2 = x^3 + ax + b (mod p)
-        const lhs = y.square().mod(p);
-        const rhs = x.pow(3).add(a.multiply(x)).add(b).mod(p);
-        return lhs.equals(rhs);
-    }
-
-    public isValid(Q) {
-        // Check Q != O
-        if (this.isInfinity(Q)) return { valid: false, error: 'Point is at infinity' };
-        if (!this.isOnCurve(Q)) return { valid: false, error: 'Point is not on the curve' };
-
-        // Check nQ = O (where Q is a scalar multiple of G)
-        const nQ = Q.multiply(this.n);
-        if (!this.isInfinity(nQ)) return { valid: false, error: 'Point is not a scalar multiple of G' };
-
-        return { valid: true };
-    }
 }
 
-export const SecpCurve = EccCurve.fromCurveName('secp256k1');
+export const SecpCurve = EccCurve.secp256k1();
